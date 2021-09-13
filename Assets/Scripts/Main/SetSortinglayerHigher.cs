cbe74a49e4c98c5e3777f136c489d851cc294185
TheYoungSatan <loltheyoungsatan@gmail.com>
2021-09-13 19:00:48 +0200

    Decorated the room more for start levell

---
+++ Assets/Scripts/Main/SetSortinglayerHigher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SetSortinglayerHigher : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _compareRenderer;
+    private SpriteRenderer _selfRenderer;
+
+    private void Start()
+    {
+        _selfRenderer = GetComponent<SpriteRenderer>();
+        _selfRenderer.sortingOrder = _compareRenderer.sortingOrder + 1;
+    }
+}
