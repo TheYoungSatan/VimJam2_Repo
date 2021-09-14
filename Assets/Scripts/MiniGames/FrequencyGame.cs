c8d0545714350e14c1a98ad1b0d447cca2c8b522
Thorvald <thorvaldmortele@hotmail.be>
2021-09-14 19:21:53 +0200

    FrequencyGame 1
    addd folder and script

---
+++ Assets/Scripts/MiniGames/FrequencyGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniGame
+{
+    public class FrequencyGame : Minigame
+    {
+
+        public override void RunGame()
+        {
+            base.RunGame();
+        }
+
+        public override void UpdateGame()
+        {
+            base.UpdateGame();
+        }
+
+        public override void CheckInput()
+        {
+            base.CheckInput();
+        }
+    }
+
+}
