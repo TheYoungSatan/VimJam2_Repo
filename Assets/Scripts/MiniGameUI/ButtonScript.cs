742339e8b58e0920da39c8c07bbdf506dff1ffec
Koen <Laureys.koen@hotmail.be>
2021-09-14 22:41:43 +0200

    Base of MiniGameUI implemented

---
+++ Assets/Scripts/MiniGameUI/ButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private GameObject _previewMiniGame;
+
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // enable preview of hovered button
+        _previewMiniGame.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // disable preview of hovered button
+        _previewMiniGame.SetActive(false);
+    }
+
+
+    public void OnButtonClickedPart1DisableCurrent(GameObject panelToDisable)
+    {
+        panelToDisable.SetActive(false);
+    }
+    public void OnButtonClickedPart2EnableNew(GameObject panelToEnable)
+    {
+        panelToEnable.SetActive(true);
+    }
+
+}
