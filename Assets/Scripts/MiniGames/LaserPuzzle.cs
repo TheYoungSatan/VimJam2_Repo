d01acb59559fcf5b30bd7a55c451804feb9b7b29
Thorvald <thorvaldmortele@hotmail.be>
2021-09-14 10:18:56 +0200

    LaserPuzzle 1
    added laserlogic, for easy difficulty

--- Assets/Scripts/MiniGames/LaserPuzzle.cs
+++ Assets/Scripts/MiniGames/LaserPuzzle.cs
@@ -1,11 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace MiniGame
 {
     public class LaserPuzzle : Minigame
     {
+        #region Input
+        public void InputMouseClick(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                var scaledMousePos = Camera.main.ScreenToWorldPoint(_mousePos);
+                var correctMousePos = new Vector3(scaledMousePos.x, scaledMousePos.y, 0);
+
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(correctMousePos.x, correctMousePos.y), Vector2.zero, 0);
+                if (hit)
+                {
+                    if (hit.collider.CompareTag("Mirror"))
+                    {
+                        hit.collider.gameObject.transform.Rotate(0, 0, 90);
+                    }
+                }
+            }
+        }
+        #endregion
+
         public int Reflections;
         public float MaxLength;
 
@@ -15,8 +36,19 @@ namespace MiniGame
         private GameObject _laserPointer;
         private Ray2D _ray;
         private RaycastHit2D _hit;
-        private Vector3 _direction;
 
+        private Vector2 _mousePos;
+        private PlayerInput _playerInput;
+        private InputAction _clickAction;
+
+        private void Awake()
+        {
+            _playerInput = FindObjectOfType<PlayerInput>();
+
+            _clickAction = _playerInput.currentActionMap.FindAction("MouseClick");
+
+            _clickAction.performed += InputMouseClick;
+        }
 
         public override void RunGame()
         {
@@ -25,11 +57,10 @@ namespace MiniGame
 
         public override void UpdateGame()
         {
-            var origin = new Vector2(_laserPointer.transform.position.x, _laserPointer.transform.position.y);
-            _ray = new Ray2D(origin, _laserPointer.transform.right);
+            _ray = new Ray2D(_laserPointer.transform.position, _laserPointer.transform.right);
 
             _lineRenderer.positionCount = 1;
-            _lineRenderer.SetPosition(0, origin);
+            _lineRenderer.SetPosition(0, _laserPointer.transform.position);
             float remainingLength = MaxLength;
 
             for (int i = 0; i < Reflections; i++)
@@ -37,11 +68,11 @@ namespace MiniGame
                 _hit = Physics2D.Raycast(_ray.origin, _ray.direction, remainingLength);
                 if (_hit)
                 {
+                    remainingLength -= Vector2.Distance(_ray.origin, _hit.point);
                     _lineRenderer.positionCount += 1;
-                    var hitpoint = new Vector2(_hit.point.x, _hit.point.y);
-                    _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, hitpoint);
-                    remainingLength -= Vector2.Distance(_ray.origin, hitpoint);
-                    _ray = new Ray2D(hitpoint, Vector2.Reflect(_ray.direction, _hit.normal));
+                    _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, _hit.point);
+                    
+                    _ray = new Ray2D(_hit.point - _ray.direction * 0.01f, Vector2.Reflect(_ray.direction, _hit.normal));
 
                     if (_hit.collider.tag != "Mirror")
                     {
@@ -58,7 +89,7 @@ namespace MiniGame
 
         public override void CheckInput()
         {
-            base.CheckInput();
+            _mousePos = Mouse.current.position.ReadValue();
         }
     }
 
