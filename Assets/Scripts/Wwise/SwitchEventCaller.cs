783e3be4be701d96787ca44798a26078aae834c4
TheYoungSatan <loltheyoungsatan@gmail.com>
2021-09-14 21:09:25 +0200

    Custom trigger text

---
+++ Assets/Scripts/Wwise/SwitchEventCaller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SwitchEventCaller : AkTriggerBase
+{
+    [SerializeField] private GameObject _checkObject;
+    
+    private void Awake()
+    {
+        if (_checkObject == null)
+            _checkObject = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != _checkObject)
+            return;
+
+        if (triggerDelegate != null)
+            triggerDelegate(_checkObject);
+    }
+}
