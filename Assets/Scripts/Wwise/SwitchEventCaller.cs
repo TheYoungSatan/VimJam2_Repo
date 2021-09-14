bc75315d756f3984864a3a6ead59a57bfe84323d
TheYoungSatan <loltheyoungsatan@gmail.com>
2021-09-14 22:34:05 +0200

    Bureauscene change

--- Assets/Scripts/Wwise/SwitchEventCaller.cs
+++ Assets/Scripts/Wwise/SwitchEventCaller.cs
@@ -2,11 +2,27 @@
 
 public class SwitchEventCaller : AkTriggerBase
 {
+    [SerializeField] private GameObject _checkObject;
+    public bool _checkTrigger = false;
+
+    private void Awake()
+    {
+        if (!_checkObject)
+            _checkObject = FindObjectOfType<PlayerController>().gameObject;
+    }
+
     public void CallSwitch()
     {
         if (triggerDelegate != null)
         {
             triggerDelegate(this.gameObject);
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!_checkTrigger) return;
+
+        if (collision.gameObject == _checkObject)
+            CallSwitch();
     }
 }
