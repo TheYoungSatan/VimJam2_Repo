adb58d6acf62411dcac0613cedd158e5c6c214d1
TheYoungSatan <loltheyoungsatan@gmail.com>
2021-09-15 21:34:02 +0200

    Script change push

--- Assets/Scripts/Audio/BackgroundAudio.cs
+++ Assets/Scripts/Audio/BackgroundAudio.cs
@@ -7,6 +7,7 @@ public class BackgroundAudio : MonoBehaviour
     private SwitchEventCaller _caller;
     private void Start()
     {
+        _caller = GetComponent<SwitchEventCaller>();
         _caller.CallSwitch();
         AudioHub.PlaySound(AudioHub.BackgroundMusic);
     }
