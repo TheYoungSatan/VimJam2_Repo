b4be27b6dd7e8d89f2711725f4602b3ecc5740bd
TheYoungSatan <loltheyoungsatan@gmail.com>
2021-09-18 15:11:03 +0200

    Changeable tilemap based on succession

---
+++ Assets/Scripts/Visuals/ChangeableTile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum VisualCompletionLevel { Easy, Medium, Hard, Failed}
+
+[CreateAssetMenu(fileName = "new changeable tile", menuName = "Tiles/ Changeable Tile")]
+public class ChangeableTile : Tile
+{
+    public Sprite Easy;
+    public Sprite Medium;
+    public Sprite Hard;
+    private Sprite _failed;
+
+    private VisualCompletionLevel _completionLevel;
+    private Sprite _newSprite;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        base.GetTileData(position, tilemap, ref tileData);
+
+        if(GameChanger.instance)
+            _completionLevel = GameChanger.instance.CompletionLevel;
+
+        switch (_completionLevel)
+        {
+            case VisualCompletionLevel.Easy:
+                _newSprite = Easy;
+                break;
+            case VisualCompletionLevel.Medium:
+                _newSprite = Medium;
+                break;
+            case VisualCompletionLevel.Hard:
+                _newSprite = Hard;
+                break;
+            case VisualCompletionLevel.Failed:
+                CreateFailedSprite();
+                _newSprite = _failed;
+                break;
+        }
+        tileData.sprite = _newSprite;
+    }
+
+    private void CreateFailedSprite()
+    {
+        Texture2D texture = new Texture2D(Easy.texture.width, Easy.texture.height);
+        for (int x = 0; x < texture.width; x++)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                int r = UnityEngine.Random.Range(0, 2);
+
+                if (r == 0)
+                    texture.SetPixel(x, y, Color.white);
+                else 
+                {
+                    Color color = new Color(Easy.texture.GetPixel(x, y).r, Easy.texture.GetPixel(x, y).g, Easy.texture.GetPixel(x, y).b, 1);
+                    texture.SetPixel(x, y, color);
+                }
+            }
+        }
+        _failed = Sprite.Create(texture, Easy.textureRect, Easy.pivot); 
+    }
+}
---
+++ Assets/Scripts/Visuals/GameChanger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameChanger : MonoBehaviour
+{
+    public static GameChanger instance;
+    public VisualCompletionLevel CompletionLevel;
+    private Tilemap _gameMap;
+
+    private void Start()
+    {
+        instance = this;
+        _gameMap = FindObjectOfType<Tilemap>();
+        if (_gameMap)
+            _gameMap.RefreshAllTiles();
+
+    }
+}
