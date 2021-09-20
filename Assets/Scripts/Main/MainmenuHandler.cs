84a405fcd68ed7de88eebec1845f92ae0e94d9c9
TheYoungSatan <loltheyoungsatan@gmail.com>
2021-09-20 17:32:23 +0200

    Auto stash before rebase of "origin/main"

--- Assets/Scripts/Main/MainmenuHandler.cs
+++ Assets/Scripts/Main/MainmenuHandler.cs
@@ -2,11 +2,20 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainmenuHandler : MonoBehaviour
 {
+    [SerializeField] private Slider _master;
+    [SerializeField] private Slider _music;
+    [SerializeField] private Slider _sfx;
+
     private void Start()
     {
+        _master.value = .75f;
+        _music.value = 1f;
+        _sfx.value = 1f;
+        ChangeVolume();
         GameInfo.ResetInfo();
         GUI.SetActive(false);
     }
@@ -20,6 +29,13 @@ public class MainmenuHandler : MonoBehaviour
     {
         GameInfo.ResetInfo();
         StartCoroutine(LoadGame(SceneName));
+    }
+
+    public void ChangeVolume()
+    {
+        AkSoundEngine.SetRTPCValue("MasterVolume", _master.value);
+        AkSoundEngine.SetRTPCValue("MusicVolume", _music.value);
+        AkSoundEngine.SetRTPCValue("SFXVolume", _sfx.value);
     }
 
     public void QuitGame() => Application.Quit();
