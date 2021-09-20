72bb529bf58075a3924c370fe1d20b8b34426104
TheYoungSatan <loltheyoungsatan@gmail.com>
2021-09-20 19:03:49 +0200

    Final push

--- Assets/Scripts/Main/GameOverPanel.cs
+++ Assets/Scripts/Main/GameOverPanel.cs
@@ -9,14 +9,14 @@ public class GameOverPanel : MonoBehaviour
 
     public void SetText(PlayerInfo info)
     {
-        string quitText = $"Hunger: {info.HungerPercentage}% \nThurst: {info.ThurstPercentage}% \nTime awake: {info.AwakeTime}h";
+        string quitText = $"Hunger: {info.HungerPercentage}% \nThirst: {info.ThurstPercentage}% \nTime awake: {info.AwakeTime}h";
 
         if (info.AwakeTime >= 30)
             quitText += "\ndue to over exhaustion";
         if (info.HungerPercentage >= 100)
             quitText += "\ndue to being to hungry";
         if (info.ThurstPercentage >= 100)
-            quitText += "\ndue to being to thursty";
+            quitText += "\ndue to being to thirsty";
 
         explenationtext.text = quitText;
     }
